7426aa05325d30eca5690b83b3b4c938dc4eb0b4
seven89 <compiler@compiler-laptop>
2012-05-23 15:39:03 +0200

    Zwischencode update..

--- dhbwCompiler/src/ir_code_generation.c
+++ dhbwCompiler/src/ir_code_generation.c
@@ -2,7 +2,7 @@
  * ir_code_generation.c
  *
  *  Created on: Apr 3, 2012
- *      Author: FS
+ *      Author: Fabian Schaefer
  */
 
 #include "ir_code_generation.h"
@@ -16,10 +16,10 @@ int num_of_codes;
 
 //Initialize
 ir_code *code_list = NULL;
-TRUELIST  *truelist = NULL;
-FALSELIST *falselist = NULL;
+ir_code *truelist = NULL;
+ir_code *falselist = NULL;
 
-void genQuad(enum code_ops operation, struct symbol *address_0,
+void genQuad(enum code_ops *operation, struct symbol *address_0,
 		struct symbol *address_1, struct symbol *address_2, int jmpTo) {
 	/**Quadrupel Code Generation**/
 	ir_code *c = NULL;
@@ -41,211 +41,63 @@ void print_all_codes(){
 	}
 }
 
+void addReturn()
+{
+	//???
+}
+void addAssign(enum code_ops *op, struct symbol *var, struct symbol *value)
+{
+	genQuad(op,var,value,NULL,NULL);
+}
 void addCalc(enum code_ops *op, struct symbol *result, struct symbol *exp1,
 		struct symbol *exp2)
 {
 	genQuad(op, result, exp1, exp2,NULL);
 }
-void addWhile(struct symbol *var, enum code_ops *op, struct symbol *condition,
-		struct symbol *step, int label);
-void addWhileDo(struct symbol *var, enum code_ops *op, struct symbol *condition,
-		struct symbol *step, int label);
-void addIf(enum code_ops *op, struct symbol *expressions, struct symbol *marker)
+void addWhile(enum code_ops *op, struct symbol *exp1, struct symbol *exp2)
 {
-	int exp = 0;
-	/**expressions is an array of expressions
-	 * marker is the jump address, maybe an array of length two*/
-	if(sizeof(expressions) <= 1)
+	/**After each loop iteration, the GOTO label points to the row in before the while-header and
+	 * the while condition is checked again...
+	 * In case of a false while condition the row after the while loop will be aimed
+	 * So for the truelist entry, the GOTO label at the end of the while loop is needed
+	 */
+	falselist->FALSELIST.row_id = code_list->code.counter_id;
+	genQuad(op, exp1, exp2, NULL, NULL);
+}
+void addWhileDo(enum code_ops *op, struct symbol *exp1, struct symbol *exp2);
+void addIf(enum code_ops *op, struct symbol *exp1, struct symbol *exp2)
+{
+	/**While calling the addif function a jmpto parameter is not necessary, because it would be always NULL for the first time**/
+	//save rows for jumps in order to backpatch the right jump address later
+	truelist->TRUELIST.row_id = code_list->code.counter_id;
+	falselist->FALSELIST.row_id = code_list->code.counter_id+1;
+	genQuad(op, exp1, exp2, NULL,NULL);
+}
+
+void addGoTo();
+void addFunc(enum code_ops *op, struct symbol *type, struct symbol *name,
+		int countParam);
+
+void mergeBLists ()
+{
+	/**This function handles multiple if-operations and merges therefore true-/falselists**/
+}
+void backpatch (int *marker, int check)
+{
+	/** marker is real jump address; check decides for true-/falselist --> 1:true; 0:false
+	 * At first one searches the row in the ir_code by using the true-/falselists.
+	 * Afterwards the real jump address is filled in...
+	**/
+	//TODO: consideration of mergeBLists
+	if(check == 1)
 	{
-		/** simple if-statement with only one expression**/
-		genQuad(op, exp, NULL, NULL, marker);
+		//TODO: getting right row of truelist
+		//TODO: putting real jump address to ir_code
 	}
 	else
 	{
-		/** more complex if-statement with more than one expression**/
-		//get each exp of expressions array
-		while(1)
-		{
-			if(exp == sizeof(expressions)-1)
-			{
-				//TODO: decision whether true-/falselist
-				genQuad(op, NULL, NULL, NULL, get_marker(*truelist,true));
-			}
-			else
-			{
-				backpatch(expressions[exp], marker);
-			}
-		}
+
 	}
-}
 
-symbol get_marker(ir_code *list, boolean checkList)
-{
-	/**get_marker returns the last input marker of a true-/false list**/
-	ir_code *torf = NULL;
-	LL_FOREACH(list,torf);{
-		if(checkList == true)
-		{
-			if(torf->TRUELIST.counter_id == sizeof(list)-1)
-			{
-				return torf;
-			}
-		}
-		else
-		{
-			if(torf->FALSELIST.counter_id == sizeof(list)-1)
-			{
-				return torf;
-			}
-		}
-	}
-}
-
-void backpatch (struct symbol *exp, struct symbol *marker)
-{
-	//TODO decision for true-/falselist
-	//Initialize
-	ir_code *list = NULL;
-	list = (struct ir_code*) malloc(sizeof(struct ir_code));
-	/**save to TrueList**/
-	list->TRUELIST.marker = marker;
-	list->TRUELIST.exp = exp;
-	LL_APPEND(truelist,list);
-	/**save to FalseList**/
-	list->FALSELIST.marker = marker;
-	list->FALSELIST.exp = exp;
-	LL_APPEND(falselist,list);
 
 }
-void addIfGoTo();
-void addFunc(enum code_ops *op, struct symbol *type, struct symbol *name,
-		int countParam);
-void addReturn();
-void addAssign(struct symbol *res, struct symbol *val, enum code_ops *op);
-
-////temporary array; dimension is given by count
-//symbol irtemp(int count) {
-//	int size = count;
-//	struct symbol *temp;
-//	temp = (symbol *) malloc(size * sizeof(symbol));
-//	if (temp == NULL) {
-//		printf("Error with malloc...\n");
-//		//TODO: further steps in case error
-//	}
-//	return *temp;
-//}
-////zwischenvariable im parser nach oben geben
-//
-//void genQuad(enum code_ops *op, struct symbol *result, struct symbol *exp1,
-//		struct symbol *exp2, struct symbol *exp3, int label, int count) {
-//	//TODO: Changes to symboltable and error correction of array-assignments
-//	//dynamic array allocation
-//	//changes necessary to symboltable ???
-//	*tempArray = irtemp(count);
-//	//Building quadrupelcode: 1:result; 2:op; 3:operand; 4:operand
-//	//tempArray[0] = &(symbol)result;
-//	//tempArray[1] = (symbol)op;
-//	//tempArray[2] = (symbol)exp1;
-//	//tempArray[3] = (symbol)exp2;
-//}
-//
-//void addCalc(enum code_ops *op, struct symbol *result, struct symbol *exp1,
-//		struct symbol *exp2) {
-//	genQuad(op, result, exp1, exp2, NULL, NULL, 4);
-//	//TODO:print();
-//	//TODO:overwrite value in symboltable
-//	//TODO:free allocation
-//}
-//
-//void addWhile(struct symbol *var, enum code_ops *op, struct symbol *condition,
-//		struct symbol *step, int label) {
-//	//ob cond true /false --> sprünge + wenn schleife zu ende wieder an anfang springen
-//	/**At first the condition is checked for the first time.
-//	 * Afterwards quadrupel code is generated in order to change the checked condition variable with defined "step" e.g. i-- or i++
-//	 * During Code generation the value of var has changed in symboltable and the new condition is backpatched before addwhile is called again
-//	 * in case of a true condition...
-//	 **/
-//	//step is a value, which manipulates the var, which builds the result of the condition
-//	//label equals "1" iff one loop iteration is ready
-//	if (condition == 0) {
-//		//do not entry loop
-//		printf("while_loop is ready; zero times");
-//	} else {
-//		//jump into loop
-//		genQuad(op, var, step, NULL, NULL, label, 3);
-//		if (label == 1) {
-//			//TODO: Backpatching
-//			//*condition=backpatch();
-//			//Error: Undefined reference for addwhile-->???
-//			//addwhile(var,op,condition,step,label);
-//		} else {
-//			printf("while_loop is ready; n-times");
-//		}
-//	}
-//}
-//
-//void addWhileDo(struct symbol *var, enum code_ops *op, struct symbol *condition,
-//		struct symbol *step, int label) {
-//	/** Difference to While-Loop: the loop has minimal one iteration**/
-//	//TODO: Backpatching
-//	//*condition=backpatch();
-//	if (condition == 0) {
-//		printf("while_loop is ready; 1-time");
-//	} else {
-//		//Error: Undefined reference for addwhile-->???
-//		//addwhile(var,op,condition,step,label);
-//	}
-//
-//}
-//
-//void addIf(enum code_ops *op, struct symbol *condition, int label) {
-//	/**Assumption: Else equals If for a compiler**/
-//	/**label is a marker, which let the compiler know, when the last condition is read. **/
-//	if (label == 0) {
-//		if (condition == 0) {
-//			backpatch(); //TODO: Backpatching --> FalseList
-//		}
-//		if (condition == 1) {
-//			backpatch(); //TODO: Backpatching --> TrueList
-//		}
-//	} else {
-//		printf("if-statement is ready...");
-//	}
-//}
-//
-//void addIfGoTo() {
-//	//???
-//}
-//
-//void backpatch() {
-////true&false list
-//}
-//
-//void addFunc(enum code_ops *op, struct symbol *type, struct symbol *name,
-//		int countParam) {
-//	//???
-//}
-//
-//void addReturn() {
-//	//???
-//}
-//
-//void addAssign(struct symbol *res, struct symbol *val, enum code_ops *op) {
-//	//normal assignment
-//	if (&op == NULL) {
-//		*res = *val;
-//	}
-////	else if (&op == MINUS)
-////	{
-////		//negative assignment
-////		genQuad(op, res, val, NULL, NULL, NULL, 4);
-////	}
-//}
-//
-////void printRes()
-////{
-////for(int i = 0; i<4; i++)
-////{
-////printf("%symbol",(tempArray+i));
-////}
-////}
--- dhbwCompiler/src/ir_code_generation.h
+++ dhbwCompiler/src/ir_code_generation.h
@@ -2,7 +2,7 @@
  * ir_code_generation.h
  *
  *  Created on: Apr 3, 2012
- *      Author: FS
+ *      Author: Fabian Schaefer
  */
 
 #ifndef _DHBWCC_IR_CODE_GENERATION_H
@@ -14,8 +14,7 @@ enum code_ops {
 	opSUB,
 	opMUL,
 	opMINUS,
-	opSHIFT_LEFT,
-	opSHIFT_RIGHT,
+
 
 	opLOGICAL_AND,
 	opLOGICAL_OR,
@@ -23,9 +22,9 @@ enum code_ops {
 	opNE,
 	opEQ,
 	opGT,
-	opGTEQ,
-	opLS,
-	opLSEQ,
+	opGE,
+	opLT,
+	opLE,
 
 	opIF,
 	opGOTO,
@@ -35,51 +34,33 @@ enum code_ops {
 	opRETURN,
 	opPARAM,
 	opCALL,
-	opMEM_LD,
-	opMEM_ST,
-	opADDR,
 	opFUNC_DEF,
 	opFUNC_DEF_END,
-	opNOP
-
 };
 
 typedef struct ir_code{
 	/**ir_code pre-definitions**/
-	union code
+	struct code
 	{
 		int counter_id;
-		enum code_ops operation;
+		enum code_ops *operation;
 		struct symbol *address_0;
 		struct symbol *address_1;
 		struct symbol *address_2;
 		int jmpLabel;
 		int jmpTo;
-		union code *next;
+		struct code *next;
 	}code;
-
-	union funcCall
-	{
-		int counter_params;
-	}funcCall;
-
-	union ifExp
-	{
-		enum code_ops operation;
-	}ifExp;
 
 	union TRUELIST
 	{
-		int counter_id;
-		struct symbol *marker;
+		int row_id;
 		union TRUELIST *next;
 	}TRUELIST;
 
 	union FALSELIST
 	{
-		int counter_id;
-		struct symbol *marker;
-		struct symbol *exp;
+		int row_id;
 		union FALSELIST *next;
 	}FALSELIST;
 
@@ -87,32 +68,35 @@ typedef struct ir_code{
 }ir_code;
 
 static char* enumStrings[] = { "ASSIGN", "ADD", "SUB", "MUL", "MINUS",
-		"SHIFT_LEFT", "SHIFT_RIGHT",
 
-		"LOGICAL_AND", "LOGICAL_OR", "LOGICAL_NOT", "NE", "EQ", "GT", "GTEQ",
-		"LS", "LSEQ",
+		"LOGICAL_AND", "LOGICAL_OR", "LOGICAL_NOT", "NE", "EQ", "GT", "GE",
+		"LT", "LE",
 
 		"IF", "GOTO", "WHILE_BEGIN", "DO_WHILE_BEGIN",
 
-		"RETURN", "PARAM", "CALL", "MEM_LD", "MEM_ST", "ADDR", "FUNC_DEF",
-		"FUNC_DEF_END", "NOP"
+		"RETURN", "PARAM", "CALL", "FUNC_DEF",
+		"FUNC_DEF_END"
 
 };
 
 void print_all_codes();
-
+void addAssign(enum code_ops *op, struct symbol *var, struct symbol *value);
 void addCalc(enum code_ops *op, struct symbol *result, struct symbol *exp1,
 		struct symbol *exp2);
-void addWhile(struct symbol *var, enum code_ops *op, struct symbol *condition,
-		struct symbol *step, int label);
-void addWhileDo(struct symbol *var, enum code_ops *op, struct symbol *condition,
-		struct symbol *step, int label);
-void addIf(enum code_ops *op, struct symbol *expressions, struct symbol *marker);
+void addIf(enum code_ops *op, struct symbol *exp1, struct symbol *exp2);
+
+
+void addWhile(enum code_ops *op, struct symbol *exp1, struct symbol *exp2);
+void addWhileDo(enum code_ops *op, struct symbol *exp1, struct symbol *exp2);
+
 void addIfGoTo();
 void addFunc(enum code_ops *op, struct symbol *type, struct symbol *name,
 		int countParam);
 void addReturn();
-void addAssign(struct symbol *res, struct symbol *val, enum code_ops *op);
+
+
+
+
 
 #endif
 
